e845ebee29eafc40aa9b7f8b8b2c522d10793886
Nick Wells <blazesk8er82@gmail.com>
2012-04-19 02:37:12 -0400

    First commit

--- Add-Firestorm-rom-as-repo.sh
+++ Add-Firestorm-rom-as-repo.sh
@@ -1,10 +1,10 @@
 #This script is going to just be very simple and initialize a .repo from CM until we have our own and then it syncs the github of Firestormbase
 echo "now pulling down source"
+cd ~/
 git clone git://github.com/xIndirect/Firestormbase.git
 cd Firestormbase
 repo init -u git://github.com/CyanogenMod/android.git
 echo Now you're setup. Congratulations.
 #Use github to push changes
 #Repo tool had to be used because of device files that CM didnt includ for make
-#We wouldn't be able to pull files without that .repo folder.
-
+#We wouldnt be able to pull files without that .repo folder.
